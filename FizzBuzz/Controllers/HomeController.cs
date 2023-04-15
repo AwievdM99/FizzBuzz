d15bfa02b7f7bd3822a80bc63158e1b9f5d7fb17
AwievdM99 <awie.99.vandermerwe@gmail.com>
2023-04-15 04:05:21 +0200

    Added comments

--- FizzBuzz/Controllers/HomeController.cs
+++ FizzBuzz/Controllers/HomeController.cs
@@ -15,13 +15,16 @@ namespace FizzBuzz.Controllers
 
         public IActionResult Index()
         {
+            //Login behind how FizzBuzz is determined.
             List<string> fizzBuzzCalculationList = new List<string>();
             for (int i = 1; i <= 100; i++)
             {
+                //3 and 5 are checked first to prevent a false trigger from either 3 or 5 which would give a false value.
                 if (i % 3 == 0 && i % 5 == 0)
                 {
                     fizzBuzzCalculationList.Add("FizzBuzz");
                 }
+                //The order in which 3 and 5 are checked does not matter because 3 is not a multiple of 5 and vice versa. 
                 else if (i % 3 == 0)
                 {
                     fizzBuzzCalculationList.Add("Fizz");
@@ -30,11 +33,13 @@ namespace FizzBuzz.Controllers
                 {
                     fizzBuzzCalculationList.Add("Buzz " + i);
                 }
+                //If nothing is triggered, then the number is added to the list instead.
                 else
                 {
                     fizzBuzzCalculationList.Add(i.ToString());
                 }
             }
+            //Viewdata is used to pass the list to the index file.
             ViewData["fizzBuzz"] = fizzBuzzCalculationList;
             return View();
         }
